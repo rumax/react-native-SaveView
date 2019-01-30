d912f621d4c3075bcb3fe41f93d3f67a4b4fc314
sander frenken <s.h.frenken@ing.nl>
2019-01-30 14:32:56 +0100

    iOS support for save image to disk and base64 finished

--- ios/RNSaveView/RNSaveView/RNSaveViewConstants.h
+++ ios/RNSaveView/RNSaveView/RNSaveViewConstants.h
@@ -1,4 +1,5 @@
 #import <Foundation/Foundation.h>
 
-// Error types
 extern NSString * const ERROR_INVALID_REACT_TAG;
+extern NSString * const ERROR_INVALID_BASE_64;
+extern NSString * const ERROR_MESSAGE_INVALID_BASE_64;
--- ios/RNSaveView/RNSaveView/RNSaveViewManager.m
+++ ios/RNSaveView/RNSaveView/RNSaveViewManager.m
@@ -4,28 +4,66 @@
 
 RCT_EXPORT_MODULE()
 
-RCT_EXPORT_METHOD(saveToPNGBase64: (nonnull NSNumber *)reactTag filePath: (nonnull NSString *)filePath resolver: (RCTPromiseResolveBlock)resolve rejecter: (RCTPromiseRejectBlock)reject) {
+RCT_EXPORT_METHOD(saveToPNGBase64: (nonnull NSNumber *)reactTag resolver: (RCTPromiseResolveBlock)resolve rejecter: (RCTPromiseRejectBlock)reject) {
   dispatch_async(dispatch_get_main_queue(), ^{
-    UIView *view = (UIView *)[self.bridge.uiManager viewForReactTag: reactTag];
-    UIImage *snapshot;
-    
-    if ([view isKindOfClass: [RCTScrollView class]]) {
-      snapshot = [self takeSnapshotOfScrollView: (RCTScrollView *)view];
-    } else {
-      snapshot = [self takeSnapshotOfView: view];
-    }
-    if (snapshot != nil) {
-      NSFileManager *fileManager = [NSFileManager defaultManager];
-      NSData *myImageData = UIImageJPEGRepresentation(snapshot, 1.0);
-      [fileManager createFileAtPath: filePath contents: myImageData attributes: nil];
-      resolve(nil);
-    } else {
-      reject(ERROR_INVALID_REACT_TAG, [NSString stringWithFormat: @"ReactTag passed: %@", reactTag], nil);
-    }
+      UIImage *snapshot = [self snapshotReactView: reactTag];
+      if (snapshot != nil) {
+          NSString* base64Image = [self imageToBase64: snapshot];
+          if (base64Image != nil) {
+              resolve(base64Image);
+          } else {
+              reject(ERROR_INVALID_BASE_64, ERROR_MESSAGE_INVALID_BASE_64, nil);
+          }
+      } else {
+          reject(ERROR_INVALID_REACT_TAG, [NSString stringWithFormat: @"ReactTag passed: %@", reactTag], nil);
+      }
   });
 }
 
-- (UIImage *)takeSnapshotOfView: (UIView *)view {
+RCT_EXPORT_METHOD(saveToPNG: (nonnull NSNumber *)reactTag filePath: (nonnull NSString *)filePath resolver: (RCTPromiseResolveBlock)resolve rejecter: (RCTPromiseRejectBlock)reject) {
+    dispatch_async(dispatch_get_main_queue(), ^{
+        UIImage *snapshot = [self snapshotReactView: reactTag];
+        if (snapshot != nil) {
+            [self saveImage: snapshot atFilepath: filePath];
+            resolve(nil);
+        } else {
+            reject(ERROR_INVALID_REACT_TAG, [NSString stringWithFormat: @"ReactTag passed: %@", reactTag], nil);
+        }
+    });
+}
+
+- (instancetype)init {
+    self = [super init];
+    return self;
+}
+
++ (BOOL)requiresMainQueueSetup {
+    return YES;
+}
+
+- (UIImage *)snapshotReactView: (nonnull NSNumber *)reactTag {
+    UIView *view = (UIView *)[self.bridge.uiManager viewForReactTag: reactTag];
+    // Store the original background color
+    UIColor* originalColor = view.backgroundColor;
+    // If the background color is not set it will default to white
+    if (originalColor == nil) {
+        view.backgroundColor = [UIColor whiteColor];
+    }
+    
+    UIImage *snapshot;
+    
+    if ([view isKindOfClass: [RCTScrollView class]]) {
+        snapshot = [self snapshotScrollView: (RCTScrollView *)view];
+    } else {
+        snapshot = [self snapshotView: view];
+    }
+    
+    // Restore the original view's background color
+    view.backgroundColor = originalColor;
+    return snapshot;
+}
+
+- (UIImage *)snapshotView: (UIView *)view {
   UIGraphicsBeginImageContext(CGSizeMake(view.frame.size.width, view.frame.size.height));
   [view drawViewHierarchyInRect: CGRectMake(0, 0, view.frame.size.width, view.frame.size.height) afterScreenUpdates: YES];
   UIImage *image = UIGraphicsGetImageFromCurrentImageContext();
@@ -34,7 +72,7 @@ RCT_EXPORT_METHOD(saveToPNGBase64: (nonnull NSNumber *)reactTag filePath: (nonnu
   return image;
 }
 
-- (UIImage *)takeSnapshotOfScrollView: (RCTScrollView *)scrollview {
+- (UIImage *)snapshotScrollView: (RCTScrollView *)scrollview {
   // Store the original frame of the scrollview to restore later
   CGRect originalFrame = scrollview.frame;
   // Set the frame of the scrollview equal to it's contentsize
@@ -45,18 +83,21 @@ RCT_EXPORT_METHOD(saveToPNGBase64: (nonnull NSNumber *)reactTag filePath: (nonnu
   UIImage* image = UIGraphicsGetImageFromCurrentImageContext();
   UIGraphicsEndImageContext();
   
+  // Set scrollview frame to original frame
   scrollview.frame = originalFrame;
   
   return image;
 }
 
-- (instancetype)init {
-  self = [super init];
-  return self;
+- (void) saveImage: (UIImage *)image atFilepath: (NSString *)filePath {
+    NSFileManager *fileManager = [NSFileManager defaultManager];
+    NSData *imageData = UIImageJPEGRepresentation(image, 1.0);
+    [fileManager createFileAtPath: filePath contents: imageData attributes: nil];
 }
 
-+ (BOOL)requiresMainQueueSetup {
-  return YES;
+- (NSString *)imageToBase64: (UIImage *)image {
+    NSData *imageData = UIImagePNGRepresentation(image);
+    return [imageData base64EncodedStringWithOptions: 0];
 }
 
 @end
